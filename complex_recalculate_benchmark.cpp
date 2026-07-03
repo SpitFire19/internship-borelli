3d301298e2823fe74462446baf9b1c3dbb2e4e04
SpitFire19 <yaroslav.koudrevitch@gmail.com>
2026-07-03 15:23:28 +0200

    Add benchmarks to test the speed of the online algorithm

--- complex_recalculate_benchmark.cpp
+++ complex_recalculate_benchmark.cpp
@@ -1,9 +1,5 @@
 #include "simulation.h"
 
-/*
-clear && g++ delaunay_sliding_window_benchmark.cpp -std=c++17 -I/home/yaroslav/gudhi-devel/src/Simplex_tree/include -I/home/yaroslav/gudhi-devel/src/Alpha_complex/include -I/home/yaroslav/gudhi-devel/src/common/include -I/usr/include/eigen3 -lgmp -lmpfr -lcsv -o main && ./main > out.txt
-*/
-
 int main() {
     vector<int> point_counts = {100, 250};
     vector<int> window_sizes = {50, 100, 150, 200, 250, 300, 400, 500};
@@ -30,20 +26,24 @@ int main() {
             }
             state.build_cech_delaunay_complex(window_cgal);
             auto vertex_handles = state.get_vertices_by_id();
+
             std::deque<Vertex_handle> window_pts(vertex_handles.begin(), vertex_handles.end());
 
             auto start = std::chrono::high_resolution_clock::now();
             for(int i = 0; i < n_points_after; ++i) {
                 Point pt = points_after[i];
-                Vertex_handle vh = state.insert_point(pt);
-                window_pts.push_back(vh);
+                Vertex_handle vh = state.add_triangulation_point(pt);
+                window_pts.push_back(vh);    
+                
                 if(window_pts.size() > window_size) {
                     Vertex_handle vh = window_pts.front();
                     window_pts.pop_front();
-                    state.erase_point(vh);
+                    int removed_point_id = state.get_vertex_id(vh);
+                    state.dt.remove(vh);
+                    state.remove_point_from_state(removed_point_id);
                 }
+                Simplex_tree st_recomputed = state.build_reference_complex();
             }
-
             auto end = std::chrono::high_resolution_clock::now();
             std::chrono::duration<double> elapsed = end - start;
             std::cerr << "Time for k=" << n_points_after <<" "<<", h="<<window_size << " : " << elapsed.count() << " seconds\n";
